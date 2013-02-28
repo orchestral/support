24afe112e388217c10ca90a85df8fbeb4b0c6010
crynobone <crynobone@gmail.com>
2013-02-28 11:31:36 +0800

    Add Orchestra\Support\Auth.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/AuthTest.php
@@ -0,0 +1,57 @@
+<?php namespace Orchestra\Support\Tests;
+
+class AuthTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		$mock = \Mockery::mock('Application')
+			->shouldReceive('instance')->andReturn(true);
+		
+		$eventMock = \Mockery::mock('Event')
+			->shouldReceive('until')->andReturn(array('admin', 'editor'));
+		\Illuminate\Support\Facades\Event::setFacadeApplication(
+			$mock->getMock()
+		);
+		\Illuminate\Support\Facades\Event::swap($eventMock->getMock());
+
+		$userMock     = new \stdClass();
+		$userMock->id = 0;
+
+		$authMock = \Mockery::mock('Illuminate\Auth\Guard')
+			->shouldReceive('user')->andReturn($userMock);
+		\Illuminate\Support\Facades\Auth::setFacadeApplication(
+			$mock->getMock()
+		);
+		\Illuminate\Support\Facades\Auth::swap($authMock->getMock());
+	}
+
+	/**
+	 * Test Orchestra\Support\Auth::roles() returning valid roles
+	 * 
+	 * @test
+	 * @group support
+	 */
+	public function testRolesMethod()
+	{
+		$expected = array('admin', 'editor');
+		$output   = \Orchestra\Support\Auth::roles();
+
+		$this->assertEquals($expected, $output);
+	}
+
+	/**
+	 * Test Orchestra\Support\Auth::is() returning valid roles
+	 * 
+	 * @test
+	 * @group support
+	 */
+	public function testIsMethod()
+	{
+		$this->assertTrue(\Orchestra\Support\Auth::is('admin'));
+		$this->assertTrue(\Orchestra\Support\Auth::is('editor'));
+		$this->assertFalse(\Orchestra\Support\Auth::is('user'));
+	}
+}
