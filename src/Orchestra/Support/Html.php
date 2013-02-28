c6197ad825d9e8837d5ad0a573b5bffd689990a4
crynobone <crynobone@gmail.com>
2013-02-28 23:13:19 +0800

    Add Orchestra\Support\Form\Fieldset and Orchestra\Support\Html.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Support/Html.php
@@ -0,0 +1,173 @@
+<?php namespace Orchestra\Support;
+
+use Illuminate\Support\Facades\Config;
+
+class Html {
+
+	/**
+	 * Cache application encoding locally to save expensive calls to Config::get().
+	 *
+	 * @var string
+	 */
+	public static $encoding = null;
+
+	/**
+	 * Generate a HTML element
+	 *
+	 * @static
+	 * @access public
+	 * @param  string $tag
+	 * @param  mixed  $value
+	 * @param  array  $attributes
+	 * @return string
+	 */
+	public static function create($tag = 'div', $value = null, $attributes = array())
+	{
+		if (is_array($value))
+		{
+			$attributes = $value;
+			$value      = null;
+		}
+
+		$content = '<'.$tag.static::attributes($attributes).'>';
+
+		if ( ! is_null($value))
+		{
+			$content .= static::entities($value).'</'.$tag.'>';
+		}
+		
+		return $content;
+	}
+
+	/**
+	 * Convert HTML characters to entities.
+	 *
+	 * The encoding specified in the application configuration file will be used.
+	 *
+	 * @static
+	 * @access public
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function entities($value)
+	{
+		if ($value instanceof Expression) return $value->get();
+		
+		return htmlentities($value, ENT_QUOTES, static::encoding(), false);
+	}
+
+	/**
+	 * Convert entities to HTML characters.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function decode($value)
+	{
+		return html_entity_decode($value, ENT_QUOTES, static::encoding());
+	}
+
+	/**
+	 * Convert HTML special characters.
+	 *
+	 * The encoding specified in the application configuration file will be used.
+	 *
+	 * @static 
+	 * @access public
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function specialChars($value)
+	{
+		return htmlspecialchars($value, ENT_QUOTES, static::encoding(), false);
+	}
+
+	/**
+	 * Create a new HTML expression instance.
+	 *
+	 * Database expressions are used to inject HTML.
+	 * 
+	 * @static
+	 * @access public
+	 * @param  string      $value
+	 * @return Expression
+	 */
+	public static function raw($value)
+	{
+		return new Expression($value);
+	}
+
+	/**
+	 * Build a list of HTML attributes from one or two array.
+	 *
+	 * @static
+	 * @access public
+	 * @param  array   $attributes
+	 * @param  array   $defaults
+	 * @return array
+	 */
+	public static function markup($attributes, $defaults = null)
+	{
+		// Special consideration to class, where we need to merge both string from
+		// $attributes and $defaults and take union of both.
+		$c1       = isset($defaults['class']) ? $defaults['class'] : '';
+		$c2       = isset($attributes['class']) ? $attributes['class'] : '';
+		$classes  = explode(' ', trim($c1.' '.$c2));
+		$current  = array_unique($classes);
+		$excludes = array();
+
+		foreach ($current as $c)
+		{
+			if (starts_with($c, '!'))
+			{
+				$excludes[] = substr($c, 1);
+				$excludes[] = $c;
+			}
+		}
+
+		$class      = implode(' ', array_diff($current, $excludes));
+		$attributes = array_merge($defaults, $attributes);
+
+		empty($class) or $attributes['class'] = $class;
+
+		return $attributes;
+	}
+
+	/**
+	 * Build a list of HTML attributes from an array.
+	 *
+	 * @param  array   $attributes
+	 * @return string
+	 */
+	public static function attributes($attributes)
+	{
+		$html = array();
+
+		foreach ((array) $attributes as $key => $value)
+		{
+			// For numeric keys, we will assume that the key and the value are the
+			// same, as this will convert HTML attributes such as "required" that
+			// may be specified as required="required", etc.
+			if (is_numeric($key)) $key = $value;
+
+			if ( ! is_null($value))
+			{
+				$html[] = $key.'="'.static::entities($value).'"';
+			}
+		}
+
+		return (count($html) > 0) ? ' '.implode(' ', $html) : '';
+	}
+
+	/**
+	 * Get the appliction.encoding without needing to request it from Config::get() each time.
+	 *
+	 * @static
+	 * @access protected
+	 * @return string
+	 */
+	protected static function encoding()
+	{
+		return static::$encoding ?: static::$encoding = Config::get('application.encoding');
+	}
+}
---
+++ tests/HtmlTest.php
@@ -0,0 +1,142 @@
+<?php namespace Orchestra\Support\Tests;
+
+class HtmlTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp() 
+	{
+		$appMock = \Mockery::mock('Application')
+			->shouldReceive('instance')->andReturn(true);
+
+		$configMock = \Mockery::mock('Config')
+			->shouldReceive('get')
+				->with('application.encoding')->andReturn('UTF-8');
+
+		\Illuminate\Support\Facades\Config::setFacadeApplication($appMock->getMock());
+		\Illuminate\Support\Facades\Config::swap($configMock->getMock());
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown() 
+	{
+		\Mockery::close();
+	}
+	/**
+	 * Test Orchestra\Support\Html::create() with content
+	 * 
+	 * @test
+	 * @group support
+	 */
+	public function testCreateWithContent()
+	{
+		$expected = '<div class="foo">Bar</div>';
+		$output   = \Orchestra\Support\Html::create('div', 'Bar', array('class' => 'foo'));
+
+		$this->assertEquals($expected, $output);
+	}
+
+	/**
+	 * Test Orchestra\Support\Html::create() without content
+	 * 
+	 * @test
+	 * @group support
+	 */
+	public function testCreateWithoutContent()
+	{
+		$expected = '<img src="hello.jpg" class="foo">';
+		$output   = \Orchestra\Support\Html::create('img', array(
+			'src' => 'hello.jpg', 
+			'class' => 'foo',
+		));
+
+		$this->assertEquals($expected, $output);
+
+		$expected = '<img src="hello.jpg" class="foo">';
+		$output   = \Orchestra\Support\Html::create('img', null, array(
+			'src' => 'hello.jpg', 
+			'class' => 'foo',
+		));
+
+		$this->assertEquals($expected, $output);
+	}
+
+	/**
+	 * Test Orchestra\Support\Html::entities() method
+	 *
+	 * @test
+	 * @group support
+	 */
+	public function testEntitiesMethod()
+	{
+		$output = \Orchestra\Support\Html::raw('<img src="foo.jpg">');
+		$this->assertEquals('<img src="foo.jpg">', 
+			\Orchestra\Support\Html::entities($output));
+
+		$output = '<img src="foo.jpg">';
+		$this->assertEquals('&lt;img src=&quot;foo.jpg&quot;&gt;', 
+			\Orchestra\Support\Html::entities($output));
+	}
+
+	/**
+	 * Test Orchestra\Support\Html::decode() method.
+	 *
+	 * @test
+	 * @group support
+	 */
+	public function testDecodeMethod()
+	{
+		$expected = '<img src="foo.jpg">';
+		$this->assertEquals($expected,
+			\Orchestra\Support\Html::decode('&lt;img src=&quot;foo.jpg&quot;&gt;'));
+	}
+
+	/**
+	 * Test Orchestra\Support\Html::specialchars() method.
+	 *
+	 * @test
+	 * @group support
+	 */
+	public function testSpecialCharsMethod()
+	{
+		$expected = '&lt;img src=&quot;foo.jpg&quot;&gt;';
+		$this->assertEquals($expected,
+			\Orchestra\Support\Html::specialChars('<img src="foo.jpg">'));
+	}
+
+	/**
+	 * Test Orchestra\Support\Html::raw() method.
+	 *
+	 * @test
+	 * @group support
+	 */
+	public function testRawExpressionMethod()
+	{
+		$output = \Orchestra\Support\Html::raw('hello');
+		$this->assertInstanceOf('\Orchestra\Support\Expression', $output);
+	}
+
+	/**
+	 * Test Orchestra\Support\Html::markup() method.
+	 *
+	 * @test
+	 * @group support
+	 */
+	public function testMarkupMethod()
+	{
+		$output   = \Orchestra\Support\Html::markup(array('class' => 'span4 table'), array('id' => 'foobar'));
+		$expected = array('id' => 'foobar', 'class' => 'span4 table');
+		$this->assertEquals($expected, $output);
+
+		$output   = \Orchestra\Support\Html::markup(array('class' => 'span4 !span12'), array('class' => 'span12'));
+		$expected = array('class' => 'span4');
+		$this->assertEquals($expected, $output);
+
+		$output   = \Orchestra\Support\Html::markup(array('id' => 'table'), array('id' => 'foobar', 'class' => 'span4'));
+		$expected = array('id' => 'table', 'class' => 'span4');
+		$this->assertEquals($expected, $output);
+	}
+}
