723eef025e62de2d864a6561235abca7fbc50bde
crynobone <crynobone@gmail.com>
2013-04-01 16:30:59 +0800

    Add Orchestra\Support\Decorator
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/DecoratorTest.php
@@ -0,0 +1,47 @@
+<?php namespace Orchestra\Support\Tests;
+
+class DecoratorTest extends \PHPUnit_Framework_TestCase {
+	
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		\Orchestra\Support\Decorator::$macros = array();
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		\Orchestra\Support\Decorator::$macros = array();
+	}
+
+	/**
+	 * Test add and using macros.
+	 *
+	 * @test
+	 * @group support
+	 */
+	public function testAddAndUsingMacros()
+	{
+		\Orchestra\Support\Decorator::macro('foo', function ()
+		{
+			return 'foo';
+		});
+
+		$this->assertEquals('foo', \Orchestra\Support\Decorator::foo());
+	}
+
+	/**
+	 * Test calling undefined macros throws an exception.
+	 *
+	 * @expectedException \BadMethodCallException
+	 * @group support
+	 */
+	public function testCallingUndefinedMacrosThrowsException()
+	{
+		\Orchestra\Support\Decorator::foobar();
+	}
+}
