08d5b9059c570c616c6a024973b3dd93f8c23f05
crynobone <crynobone@gmail.com>
2013-04-15 07:52:12 +0800

    Move all facades to support namespace.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Support/Facades/Facile.php
@@ -0,0 +1,12 @@
+<?php namespace Orchestra\Support\Facades;
+
+use \Illuminate\Support\Facades\Facade;
+
+class Facile extends Facade {
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'orchestra.facile'; }
+}
---
+++ src/Orchestra/Support/Facades/Form.php
@@ -0,0 +1,12 @@
+<?php namespace Orchestra\Support\Facades;
+
+use \Illuminate\Support\Facades\Facade;
+
+class Form extends Facade {
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'orchestra.form'; }
+}
