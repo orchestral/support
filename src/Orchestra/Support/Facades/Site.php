27cf1b1218f0c0f092e5002b1b4b49e4b5344526
crynobone <crynobone@gmail.com>
2013-04-15 11:34:12 +0800

    [#2] Remove static usage in Orchestra\Support\Site and move it to Orchestra\Foundation\Site
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Support/Facades/Site.php
@@ -0,0 +1,12 @@
+<?php namespace Orchestra\Support\Facades;
+
+use \Illuminate\Support\Facades\Facade;
+
+class Site extends Facade {
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'orchestra.site'; }
+}
