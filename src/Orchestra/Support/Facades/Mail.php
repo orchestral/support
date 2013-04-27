978cc2a8464abfa6b623754e344fd6acb535b6fa
crynobone <crynobone@gmail.com>
2013-04-27 18:29:57 +0800

    Add Orchestra\Support\Facades\Mail.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Support/Facades/Mail.php
@@ -0,0 +1,12 @@
+<?php namespace Orchestra\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Mail extends Facade {
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'orchestra.mail'; }
+}
