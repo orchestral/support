c16bb75bed6f9af74828de3a230aa826319aa65e
crynobone <crynobone@gmail.com>
2013-05-14 19:12:52 +0800

    Add Asset facade. Closes #16
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Support/Facades/Asset.php
@@ -0,0 +1,13 @@
+<?php namespace Orchestra\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Asset extends Facade {
+	
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'orchestra.asset'; }
+}
