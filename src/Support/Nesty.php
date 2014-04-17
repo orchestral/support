fb95b02bc99f2088462368073491f7fd09eed351
crynobone <crynobone@gmail.com>
2014-04-17 17:55:05 +0800

    Add `Orchestra\Support\Traits\DescendibleTrait`.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/Nesty.php
+++ src/Support/Nesty.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Fluent;
+use Orchestra\Support\Traits\DescendibleTrait;
 
 class Nesty
 {
+    use DescendibleTrait;
+
     /**
      * List of items.
      *
@@ -194,57 +197,6 @@ class Nesty
     public function is($key)
     {
         return $this->descendants($this->items, $key);
-    }
-
-    /**
-     * Get last descendant node from items recursively.
-     *
-     * @param  array    $array
-     * @param  string   $key
-     * @return \Illuminate\Support\Fluent
-     */
-    protected function descendants(array $array, $key = null)
-    {
-        if (is_null($key)) {
-            return $array;
-        }
-
-        $keys  = explode('.', $key);
-        $first = array_shift($keys);
-
-        if (! isset($array[$first])) {
-            return null;
-        }
-
-        return $this->resolveLastDecendant($array[$first], $keys);
-    }
-
-    /**
-     * Resolve last descendant node from items.
-     *
-     * @param  array    $array
-     * @param  array    $key
-     * @return \Illuminate\Support\Fluent
-     */
-    protected function resolveLastDecendant($array, $keys)
-    {
-        $isLastDescendant = function ($array, $segment) {
-            return ( ! is_array($array->childs) || ! isset($array->childs[$segment]));
-        };
-
-        // To retrieve the array item using dot syntax, we'll iterate through
-        // each segment in the key and look for that value. If it exists,
-        // we will return it, otherwise we will set the depth of the array
-        // and look for the next segment.
-        foreach ($keys as $segment) {
-            if ($isLastDescendant($array, $segment)) {
-                return $array;
-            }
-
-            $array = $array->childs[$segment];
-        }
-
-        return $array;
     }
 
     /**
