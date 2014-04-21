2cdf2015fd15a3e779e4907986915393248b540c
crynobone <crynobone@gmail.com>
2014-04-21 15:20:21 +0800

    Add Orchestra\Support\Collection.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Support/Collection.php
@@ -0,0 +1,29 @@
+<?php namespace Orchestra\Support;
+
+use Illuminate\Support\Collection as IlluminateCollection;
+
+class Collection extends IlluminateCollection implements Contracts\CsvableInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toCsv()
+    {
+        $delimiter = ',';
+        $enclosure = '"';
+        $single    = max($this->items);
+        $header    = array_keys(array_dot($single));
+
+        ob_start();
+
+        $instance = fopen('php://output', 'r+');
+
+        fputcsv($instance, $header, $delimiter, $enclosure);
+
+        foreach ($this->items as $key => $item) {
+            fputcsv($instance, array_dot($item), $delimiter, $enclosure);
+        }
+
+        return ob_get_clean();
+    }
+}
