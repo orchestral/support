eaf41e4ce06a72926e820b62c6e8ec2dea51b592
crynobone <crynobone@gmail.com>
2014-05-19 20:11:50 +0800

    Add testcase for Orchestra\Support\Traits\ObservableTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/Traits/ObservableTraitTest.php
@@ -0,0 +1,122 @@
+<?php namespace Orchestra\Support\Traits\TestCase;
+
+use Mockery as m;
+use Orchestra\Support\Traits\ObservableTrait;
+
+class ObservableTraitTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testEventDispatcher()
+    {
+        $dispatcher = m::mock('\Illuminate\Events\Dispatcher');
+
+        $this->assertNull(ObservableStub::getEventDispatcher());
+
+        ObservableStub::setEventDispatcher($dispatcher);
+
+        $this->assertEquals($dispatcher, ObservableStub::getEventDispatcher());
+
+        ObservableStub::unsetEventDispatcher();
+
+        $this->assertNull(ObservableStub::getEventDispatcher());
+    }
+
+    public function testGetObservableEventsMethod()
+    {
+        $stub1 = new ObservableStub;
+        $stub2 = new ObservableStubWithoutEvents;
+
+        $this->assertEquals(array('saving', 'saved'), $stub1->getObservableEvents());
+        $this->assertEquals(array(), $stub2->getObservableEvents());
+    }
+
+    public function testObserveWithoutDispatcher()
+    {
+        ObservableStub::flushEventListeners();
+
+        ObservableStub::observe(new FoobarObserver);
+
+        $stub = new ObservableStub;
+        $stub->save();
+
+        $this->assertFalse($stub->saving);
+        $this->assertFalse($stub->saved);
+    }
+
+    public function testObserveWithDispatcher()
+    {
+        $dispatcher = m::mock('\Illuminate\Events\Dispatcher');
+
+        $stub = new ObservableStub;
+
+        $dispatcher->shouldReceive('listen')->once()
+                ->with('saving: '.__NAMESPACE__.'\\ObservableStub', __NAMESPACE__.'\\FoobarObserver@saving')
+            ->shouldReceive('listen')->once()
+                ->with('saved: '.__NAMESPACE__.'\\ObservableStub', __NAMESPACE__.'\\FoobarObserver@saved')
+            ->shouldReceive('fire')->once()
+                ->with('saving: '.__NAMESPACE__.'\\ObservableStub', $stub)
+            ->shouldReceive('fire')->once()
+                ->with('saved: '.__NAMESPACE__.'\\ObservableStub', $stub)
+            ->shouldReceive('forget')->once()
+                ->with('saving: '.__NAMESPACE__.'\\ObservableStub')
+            ->shouldReceive('forget')->once()
+                ->with('saved: '.__NAMESPACE__.'\\ObservableStub');
+
+        ObservableStub::setEventDispatcher($dispatcher);
+
+        ObservableStub::observe(new FoobarObserver);
+
+        $stub->save();
+
+        $this->assertFalse($stub->saving);
+        $this->assertFalse($stub->saved);
+
+        ObservableStub::flushEventListeners();
+    }
+
+}
+
+class ObservableStub
+{
+    use ObservableTrait;
+
+    public $saving = false;
+
+    public $saved = false;
+
+    public function save()
+    {
+        $this->fireObservableEvent('saving', false);
+        $this->fireObservableEvent('saved', false);
+    }
+
+    public function getObservableEvents()
+    {
+        return array('saving', 'saved');
+    }
+}
+
+class ObservableStubWithoutEvents
+{
+    use ObservableTrait;
+}
+
+class FoobarObserver
+{
+    public function saving($stub)
+    {
+        $stub->saving = true;
+    }
+
+    public function saved($stub)
+    {
+        $stub->saving = true;
+    }
+}
