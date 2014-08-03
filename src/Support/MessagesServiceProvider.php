839b03686e38796d9bb045a0692a300cc9e34c14
crynobone <crynobone@gmail.com>
2014-08-03 12:31:55 +0800

    Add orchestra/messages.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/MessagesServiceProvider.php
+++ src/Support/MessagesServiceProvider.php
@@ -1,32 +1,6 @@
 <?php namespace Orchestra\Support;
 
-use Illuminate\Support\ServiceProvider;
-
-class MessagesServiceProvider extends ServiceProvider
+class MessagesServiceProvider extends \Orchestra\Messages\MessagesServiceProvider
 {
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bindShared('orchestra.messages', function ($app) {
-            return with(new Messages)->setSessionStore($app['session.store']);
-        });
-    }
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $app = $this->app;
-
-        $app->after(function () use ($app) {
-            $app['orchestra.messages']->save();
-        });
-    }
 }
