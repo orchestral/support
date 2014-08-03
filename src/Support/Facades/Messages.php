3bca8f32a884c964b950eb2140d2928e9b72e07e
crynobone <crynobone@gmail.com>
2014-08-03 12:45:54 +0800

    Replace facade and move orchestra/messages as suggest deps (and require-dev).
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/Facades/Messages.php
+++ src/Support/Facades/Messages.php
@@ -1,16 +1,6 @@
 <?php namespace Orchestra\Support\Facades;
 
-use Illuminate\Support\Facades\Facade;
-
-class Messages extends Facade
+class Messages extends \Orchestra\Messages\Facade
 {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor()
-    {
-        return 'orchestra.messages';
-    }
+
 }
