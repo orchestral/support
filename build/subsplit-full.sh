3992d49fe55eef5e48d10a0641489fb5acdfb00d
crynobone <crynobone@gmail.com>
2014-12-09 13:36:01 +0800

    Add support-providers as a component.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -1,4 +1,5 @@
 git subsplit init git@github.com:orchestral/support.git
 git subsplit publish --heads="master" src/Facades:git@github.com:orchestral/support-facades.git
+git subsplit publish --heads="master" src/Providers:git@github.com:orchestral/support-providers.git
 git subsplit publish --heads="master 2.1 2.2" src/Support:git@github.com:orchestral/support-core.git
 rm -rf .subsplit/
