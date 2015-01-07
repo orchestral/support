a9ea8cfc8d2bb738616948a68d98d2e0384ed58c
crynobone <crynobone@gmail.com>
2015-01-07 19:36:47 +0800

    Add shebang.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 git subsplit init git@github.com:orchestral/support.git
 git subsplit publish --heads="master" --no-tags src/Facades:git@github.com:orchestral/support-facades.git
 git subsplit publish --heads="master" --no-tags src/Providers:git@github.com:orchestral/support-providers.git
