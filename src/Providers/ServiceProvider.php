3dcca114aba515c0406985ba25bc839429791f1f
crynobone <crynobone@gmail.com>
2015-03-01 01:40:32 +0800

    Revert.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -1,28 +1,9 @@
 <?php namespace Orchestra\Support\Providers;
 
-use Orchestra\Contracts\Config\PackageRepository;
 use Orchestra\Support\Providers\Traits\PackageProviderTrait;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 abstract class ServiceProvider extends BaseServiceProvider
 {
     use PackageProviderTrait;
-
-    /**
-     * Merge the given configuration with the existing configuration.
-     *
-     * @param  string  $path
-     * @param  string  $key
-     * @return void
-     */
-    protected function mergeConfigFrom($path, $key)
-    {
-        parent::mergeConfigFrom($path, $key);
-
-        $config = $this->app['config'];
-
-        if ($config instanceof PackageRepository) {
-            $config->file($key);
-        }
-    }
 }
