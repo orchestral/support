15513301f9274542ab949938b77642e5859cd4b5
crynobone <crynobone@gmail.com>
2015-03-09 09:59:04 +0800

    Update subsplit.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-git subsplit init git@github.com:orchestral/support.git
+
+if [ -d .subsplit ]; then
+    git subsplit update
+else
+    git subsplit init git@github.com:orchestral/support.git
+fi
+
 git subsplit publish --heads="master 3.0" --no-tags src/Facades:git@github.com:orchestral/support-facades.git
 git subsplit publish --heads="master 3.0" --no-tags src/Providers:git@github.com:orchestral/support-providers.git
 git subsplit publish --heads="master 2.1 2.2 3.0" --no-tags src/Support:git@github.com:orchestral/support-core.git
-rm -rf .subsplit/
