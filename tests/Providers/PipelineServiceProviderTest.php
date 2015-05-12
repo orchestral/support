c15f37bc8414b0bc20056fe90704dbd081036c95
crynobone <crynobone@gmail.com>
2015-05-12 19:46:19 +0800

    Add testcase for PipelineServiceProvider/MiddlewareProviderTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/Providers/PipelineServiceProviderTest.php
@@ -0,0 +1,63 @@
+<?php namespace Orchestra\Support\Providers\TestCase; 
+
+use Illuminate\Container\Container;
+use Mockery as m;
+use Orchestra\Support\Providers\PipelineServiceProvider;
+
+class PipelineServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * Test Orchestra\Foundation\Providers\FilterServiceProvider::register()
+     * method.
+     *
+     * @test
+     */
+    public function testRegisterMethod()
+    {
+        $stub = new StubPipelineProvider(null);
+
+        $this->assertNull($stub->register());
+    }
+
+    /**
+     * Test Orchestra\Foundation\Providers\FilterServiceProvider::boot()
+     * method.
+     *
+     * @test
+     */
+    public function testBootMethod()
+    {
+        $app = new Container();
+
+        $router = m::mock('\Illuminate\Routing\Router');
+        $kernel = m::mock('\Illuminate\Contracts\Http\Kernel');
+
+        $router->shouldReceive('before')->once()->with('BeforeFilter')->andReturnNull()
+            ->shouldReceive('after')->once()->with('AfterFilter')->andReturnNull()
+            ->shouldReceive('filter')->once()->with('foo', 'FooFilter')->andReturnNull()
+            ->shouldReceive('middleware')->once()->with('foobar', 'FoobarMiddleware')->andReturnNull();
+
+        $kernel->shouldReceive('pushMiddleware')->once()->with('FooMiddleware')->andReturnNull();
+
+        $stub = new StubPipelineProvider($app);
+
+        $this->assertNull($stub->boot($router, $kernel));
+    }
+}
+
+class StubPipelineProvider extends PipelineServiceProvider
+{
+    protected $before = ['BeforeFilter'];
+    protected $after  = ['AfterFilter'];
+    protected $filters = ['foo' => 'FooFilter'];
+    protected $middleware = ['FooMiddleware'];
+    protected $routeMiddleware = ['foobar' => 'FoobarMiddleware'];
+}
