8831d57d527a2bf1fc7cfa70ac6d9fec802737a0
crynobone <crynobone@gmail.com>
2015-05-12 16:18:56 +0800

    Improves CS.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -136,7 +136,7 @@ class Str extends BaseStr
 
         // Convert hex to string.
         for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
-            $data .= chr(hexdec($hex[$i].$hex[$i+1]));
+            $data .= chr(hexdec($hex[$i].$hex[$i + 1]));
         }
 
         return $data;
