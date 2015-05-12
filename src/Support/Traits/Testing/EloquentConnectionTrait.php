336c82b647aa2a03cc14dd6c602368a361e6f433
crynobone <crynobone@gmail.com>
2015-05-12 19:06:22 +0800

    Add typehint.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/Traits/Testing/EloquentConnectionTrait.php
+++ src/Support/Traits/Testing/EloquentConnectionTrait.php
@@ -1,25 +1,26 @@
 <?php namespace Orchestra\Support\Traits\Testing;
 
 use Mockery as m;
+use Illuminate\Database\Eloquent\Model;
 
 trait EloquentConnectionTrait
 {
     /**
      * Set mock connection.
      *
-     * @param  object  $model
+     * @param  \Illuminate\Database\Eloquent\Model  $model
      */
-    protected function addMockConnection($model)
+    protected function addMockConnection(Model $model)
     {
-        $resolver = m::mock('Illuminate\Database\ConnectionResolverInterface');
+        $resolver = m::mock('\Illuminate\Database\ConnectionResolverInterface');
         $model->setConnectionResolver($resolver);
         $resolver->shouldReceive('connection')
-            ->andReturn(m::mock('Illuminate\Database\Connection'));
+            ->andReturn(m::mock('\Illuminate\Database\Connection'));
         $model->getConnection()
             ->shouldReceive('getQueryGrammar')
-            ->andReturn(m::mock('Illuminate\Database\Query\Grammars\Grammar'));
+            ->andReturn(m::mock('\Illuminate\Database\Query\Grammars\Grammar'));
         $model->getConnection()
             ->shouldReceive('getPostProcessor')
-            ->andReturn(m::mock('Illuminate\Database\Query\Processors\Processor'));
+            ->andReturn(m::mock('\Illuminate\Database\Query\Processors\Processor'));
     }
 }
