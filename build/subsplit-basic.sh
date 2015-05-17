017ffc108faf4a1676e3cb40a3d5b76ad841e276
crynobone <crynobone@gmail.com>
2015-05-18 00:45:02 +0800

    Update build script.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -6,6 +6,6 @@ else
     git subsplit init git@github.com:orchestral/support.git
 fi
 
-git subsplit publish --heads="master 3.0" --no-tags src/Facades:git@github.com:orchestral/support-facades.git
-git subsplit publish --heads="master 3.0" --no-tags src/Providers:git@github.com:orchestral/support-providers.git
-git subsplit publish --heads="master 2.1 2.2 3.0" --no-tags src/Support:git@github.com:orchestral/support-core.git
+git subsplit publish --heads="master 3.0 3.1" --no-tags src/Facades:git@github.com:orchestral/support-facades.git
+git subsplit publish --heads="master 3.0 3.1" --no-tags src/Providers:git@github.com:orchestral/support-providers.git
+git subsplit publish --heads="master 2.1 2.2 3.0 3.1" --no-tags src/Support:git@github.com:orchestral/support-core.git
