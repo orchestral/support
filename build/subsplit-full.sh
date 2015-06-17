8fec6464b860546b2c478241ba4688e9705cb119
crynobone <crynobone@gmail.com>
2015-06-17 17:54:16 +0800

    Remove 2.x branches from subsplit.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -8,4 +8,4 @@ fi
 
 git subsplit publish --heads="master 3.0 3.1" src/Facades:git@github.com:orchestral/support-facades.git
 git subsplit publish --heads="master 3.0 3.1" src/Providers:git@github.com:orchestral/support-providers.git
-git subsplit publish --heads="master 2.1 2.2 3.0 3.1" src/Support:git@github.com:orchestral/support-core.git
+git subsplit publish --heads="master 3.0 3.1" src/Support:git@github.com:orchestral/support-core.git
