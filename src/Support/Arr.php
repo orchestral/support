4147aeec80538605214751f6fe988ff8b39f365d
crynobone <crynobone@gmail.com>
2017-01-30 02:38:57 +0800

    Simplify Arr::expand().
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Support/Arr.php
+++ src/Support/Arr.php
@@ -10,25 +10,15 @@ class Arr extends BaseArr
      * Expand a dotted array. Acts the opposite way of Arr::dot().
      *
      * @param  array  $array
-     * @param  int  $depth
+     *
      * @return array
      */
-    public static function expand($array, $depth = INF)
+    public static function expand($array)
     {
         $results = [];
 
         foreach ($array as $key => $value) {
-            if (count($dottedKeys = explode('.', $key, 2)) > 1) {
-                $results[$dottedKeys[0]][$dottedKeys[1]] = $value;
-            } else {
-                $results[$key] = $value;
-            }
-        }
-
-        foreach ($results as $key => $value) {
-            if (is_array($value) && ! empty($value) && $depth > 1) {
-                $results[$key] = static::expand($value, $depth - 1);
-            }
+            static::set($results, $key, $value);
         }
 
         return $results;
