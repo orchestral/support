4cd6c74bc85c350f303c8c0192fc7a71278d3872
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-15 16:55:46 +0800

    Utilize available method.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Providers/Concerns/PackageProvider.php
+++ src/Providers/Concerns/PackageProvider.php
@@ -34,7 +34,7 @@ trait PackageProvider
      */
     public function addLanguageComponent(string $package, string $namespace, string $path): void
     {
-        $this->app->make('translator')->addNamespace($namespace, $path);
+        $this->loadTranslationsFrom($namespace, $path);
     }
 
     /**
