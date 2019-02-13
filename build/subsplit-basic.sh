d9ee0dbbe75b49fa574137054e6e327fe14be96d
Mior Muhammad Zaki <crynobone@gmail.com>
2019-02-13 09:56:15 +0800

    Prepare master branch.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -6,6 +6,6 @@ else
     git subsplit init git@github.com:orchestral/support.git
 fi
 
-git subsplit publish --heads="3.8" --no-tags src/Facades:git@github.com:orchestral/support-facades.git
-git subsplit publish --heads="3.8" --no-tags src/Providers:git@github.com:orchestral/support-providers.git
-git subsplit publish --heads="3.8" --no-tags src/Support:git@github.com:orchestral/support-core.git
+git subsplit publish --heads="master" --no-tags src/Facades:git@github.com:orchestral/support-facades.git
+git subsplit publish --heads="master" --no-tags src/Providers:git@github.com:orchestral/support-providers.git
+git subsplit publish --heads="master" --no-tags src/Support:git@github.com:orchestral/support-core.git
--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -6,6 +6,6 @@ else
     git subsplit init git@github.com:orchestral/support.git
 fi
 
-git subsplit publish --heads="3.8" src/Facades:git@github.com:orchestral/support-facades.git
-git subsplit publish --heads="3.8" src/Providers:git@github.com:orchestral/support-providers.git
-git subsplit publish --heads="3.8" src/Support:git@github.com:orchestral/support-core.git
+git subsplit publish --heads="master" src/Facades:git@github.com:orchestral/support-facades.git
+git subsplit publish --heads="master" src/Providers:git@github.com:orchestral/support-providers.git
+git subsplit publish --heads="master" src/Support:git@github.com:orchestral/support-core.git
