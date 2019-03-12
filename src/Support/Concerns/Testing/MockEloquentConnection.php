1cbeab4129c03c7b845889bd02047be05de6059c
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-12 11:35:52 +0800

    Refactor.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Support/Concerns/Testing/MockEloquentConnection.php
+++ src/Support/Concerns/Testing/MockEloquentConnection.php
@@ -11,18 +11,20 @@ trait MockEloquentConnection
      * Set mock connection.
      *
      * @param  \Illuminate\Database\Eloquent\Model  $model
+     *
+     * @return void
      */
-    protected function addMockConnection(Model $model)
+    protected function addMockConnection(Model $model): void
     {
-        $resolver = m::mock('\Illuminate\Database\ConnectionResolverInterface');
+        $resolver = m::mock('Illuminate\Database\ConnectionResolverInterface');
         $model->setConnectionResolver($resolver);
         $resolver->shouldReceive('connection')
-            ->andReturn(m::mock('\Illuminate\Database\Connection'));
+            ->andReturn(m::mock('Illuminate\Database\Connection'));
         $model->getConnection()
             ->shouldReceive('getQueryGrammar')
-            ->andReturn(m::mock('\Illuminate\Database\Query\Grammars\Grammar'));
+            ->andReturn(m::mock('Illuminate\Database\Query\Grammars\Grammar'));
         $model->getConnection()
             ->shouldReceive('getPostProcessor')
-            ->andReturn(m::mock('\Illuminate\Database\Query\Processors\Processor'));
+            ->andReturn(m::mock('Illuminate\Database\Query\Processors\Processor'));
     }
 }
